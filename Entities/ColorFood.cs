b10a626b4a3a7772ee25829e840c8a23ed075368
PhiloJoff <philojoff@gmail.com>
2021-10-05 21:55:40 +0200

    Modification des constructeurs et ajour du isActive

--- Entities/ColorFood.cs
+++ Entities/ColorFood.cs
@@ -9,11 +9,13 @@ namespace WakeUpRainbow.Entities
 {
     public class ColorFood : GameEntity
     {
-
+        private bool _isActive;
+        public bool IsActive { get => _isActive; set => _isActive = value; }
         public ColorFood(Texture2D texture2D) : base(texture2D)
         {
             _width = 5;
             _height = 5;
+            _isActive = true;
         }
 
         public ColorFood(Texture2D texture2D, Vector2 pos) : base(texture2D)
@@ -21,27 +23,32 @@ namespace WakeUpRainbow.Entities
             _width = 5;
             _height = 5;
             _pos = pos;
+            _isActive = true;
         }
-        public ColorFood(Texture2D texture2D, Vector2 pos, Color color) : base(texture2D)
+        public ColorFood(Texture2D texture2D, Vector2 pos, Color color) : this(texture2D, pos)
         {
-            _width = 5;
-            _height = 5;
-            _pos = pos;
             _color = color;
         }
+        public ColorFood(Texture2D texture2D, Vector2 pos, Color color, bool isActive) : this(texture2D, pos, color)
+        {
+            _isActive = isActive;
+        }
 
-        public ColorFood(Texture2D texture2D, int width, int height) : base(texture2D, width, height)
+        public ColorFood(Texture2D texture2D, int width, int height, Vector2 pos) : base(texture2D, width, height)
         {
             _width = width;
             _height = height;
+            _pos = pos;
+            _isActive = true;
         }
 
-        public ColorFood(Texture2D texture2D, int width, int height, Vector2 pos, Color color) : base(texture2D, width, height)
+        public ColorFood(Texture2D texture2D, int width, int height, Vector2 pos, Color color) : this(texture2D, width, height, pos)
         {
-            _width = 5;
-            _height = 5;
-            _pos = pos;
             _color = color;
+        }
+        public ColorFood(Texture2D texture2D, int width, int height, Vector2 pos, Color color, bool isActive) : this(texture2D, width, height, pos, color)
+        {
+            _isActive = isActive;
         }
     }
 }
