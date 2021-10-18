a5993d51c2a23b54af76d3d94611575e7ac164ec
jfo <jfortune@mereo.fr>
2021-10-18 23:08:14 +0200

    Add ScoreBoard Entity

---
+++ Entities/ScoreBoard.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WakeUpRainbow.Core;
+
+namespace WakeUpRainbow.Entities
+{
+    public class ScoreBoard : GameEntity
+    {
+        private string _score;
+        public string Score { get => _score; set => _score = value; }
+        public ScoreBoard() : base()
+        {
+        }
+
+        public ScoreBoard(Texture2D texture2D, int width, int height) : base(texture2D, width, height)
+        {
+        }
+
+    }
+}
