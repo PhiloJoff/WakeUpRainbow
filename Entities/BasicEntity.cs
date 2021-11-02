175172142c3725e5a9ca519069d90d663d05aba1
jfo <jfortune@mereo.fr>
2021-11-02 10:12:50 +0100

    add basicEntity

--- Entities/BasicEntity.cs
+++ Entities/BasicEntity.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using WakeUpRainbow.Core;
@@ -7,5 +8,10 @@ namespace WakeUpRainbow.Entities
 {
     class BasicEntity : GameEntity
     {
+        public BasicEntity() : base() { }
+
+        public BasicEntity(Texture2D texture, int width, int height) : base(texture, width, height) { }
+
+
     }
 }
